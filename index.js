6da954b49247e61ae49b36262c9ac88370f777fe
Tejeswara-git <eswargagansamrat@gmail.com>
2025-09-05 03:07:35 +0530

    chahed again

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("what the hell brother");
 });
